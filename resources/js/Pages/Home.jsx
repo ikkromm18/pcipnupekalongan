b5a593868ba25cf6c6bbc0f3979dea8121e2970f
ikkromm18 <muhammadikkrom749@gmail.com>
2025-05-11 17:53:00 +0700

    Set Up project

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <>
+            <h1 className="font-bold text-5xl text-center p-8">
+                Welcome To Inertia
+            </h1>
+        </>
+    );
+}
