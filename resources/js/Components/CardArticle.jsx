e9af0df47c3ccb6bab436288638f8f3004c6947a
ikkromm18 <muhammadikkrom749@gmail.com>
2025-05-14 15:33:34 +0700

    feat: finish a NewsSection on Beranda

--- resources/js/Components/CardArticle.jsx
+++ resources/js/Components/CardArticle.jsx
@@ -1,4 +1,6 @@
 import { Card } from "flowbite-react";
+import { MyAvatar } from "./MyAvatar";
+import { MyBadge } from "./MyBadge";
 
 export default function CardArticle({ title, decsription }) {
     return (
@@ -7,12 +9,19 @@ export default function CardArticle({ title, decsription }) {
             imgAlt="Meaningful alt text for an image that is not purely decorative"
             imgSrc="https://picsum.photos/id/1016/600/350"
         >
+            <div className=""></div>
+            <p className="font-light text-xs text-[#8F8F8F]">1 Hours Ago</p>
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 {title}
             </h5>
             <p className="font-normal text-gray-700 dark:text-gray-400">
                 {decsription}
             </p>
+            <div className="flex items-center gap-4">
+                <MyAvatar />
+                <p className="text-xs">Mahrus Sanjoku</p>
+            </div>
+            <MyBadge />
         </Card>
     );
 }
---
+++ resources/js/Components/MyAvatar.jsx
@@ -0,0 +1,9 @@
+import { Avatar } from "flowbite-react";
+
+export function MyAvatar() {
+    return (
+        <div className="flex flex-wrap gap-2">
+            <Avatar placeholderInitials="MS" rounded />
+        </div>
+    );
+}
