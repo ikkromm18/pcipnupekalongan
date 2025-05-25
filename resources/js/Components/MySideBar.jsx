a591ac15f22aa930ac72876993437987e6550cc2
ikkromm18 <muhammadikkrom749@gmail.com>
2025-05-25 20:12:12 +0700

    init a UI Admin

--- resources/js/Components/MySideBar.jsx
+++ resources/js/Components/MySideBar.jsx
@@ -1,7 +1,9 @@
+// MySideBar.jsx
 "use client";
 
 import {
     Sidebar,
+    SidebarLogo,
     SidebarCollapse,
     SidebarItem,
     SidebarItemGroup,
@@ -16,37 +18,95 @@ import {
     HiUser,
 } from "react-icons/hi";
 
-export default function MySideBar() {
+export default function MySideBar({ isOpen, setIsOpen }) {
     return (
-        <Sidebar aria-label="Sidebar with multi-level dropdown example">
-            <SidebarItems>
-                <SidebarItemGroup>
-                    <SidebarItem href="#" icon={HiChartPie}>
-                        Dashboard
-                    </SidebarItem>
-                    <SidebarCollapse icon={HiShoppingBag} label="E-commerce">
-                        <SidebarItem href="#">Products</SidebarItem>
-                        <SidebarItem href="#">Sales</SidebarItem>
-                        <SidebarItem href="#">Refunds</SidebarItem>
-                        <SidebarItem href="#">Shipping</SidebarItem>
-                    </SidebarCollapse>
-                    <SidebarItem href="#" icon={HiInbox}>
-                        Inbox
-                    </SidebarItem>
-                    <SidebarItem href="#" icon={HiUser}>
-                        Users
-                    </SidebarItem>
-                    <SidebarItem href="#" icon={HiShoppingBag}>
-                        Products
-                    </SidebarItem>
-                    <SidebarItem href="#" icon={HiArrowSmRight}>
-                        Sign In
-                    </SidebarItem>
-                    <SidebarItem href="#" icon={HiTable}>
-                        Sign Up
-                    </SidebarItem>
-                </SidebarItemGroup>
-            </SidebarItems>
-        </Sidebar>
+        <div
+            className={`fixed z-40 md:static ${
+                isOpen ? "block" : "hidden"
+            } md:block`}
+        >
+            <Sidebar aria-label="Sidebar" className="w-64">
+                <SidebarLogo
+                    href="#"
+                    img="./images/pcipnulogo.png"
+                    imgAlt="Flowbite logo"
+                ></SidebarLogo>
+                <SidebarItems>
+                    <SidebarItemGroup>
+                        <SidebarItem
+                            href="#"
+                            icon={HiChartPie}
+                            onClick={() => setIsOpen(false)}
+                        >
+                            Dashboard
+                        </SidebarItem>
+                        <SidebarCollapse
+                            icon={HiShoppingBag}
+                            label="E-commerce"
+                        >
+                            <SidebarItem
+                                href="#"
+                                onClick={() => setIsOpen(false)}
+                            >
+                                Products
+                            </SidebarItem>
+                            <SidebarItem
+                                href="#"
+                                onClick={() => setIsOpen(false)}
+                            >
+                                Sales
+                            </SidebarItem>
+                            <SidebarItem
+                                href="#"
+                                onClick={() => setIsOpen(false)}
+                            >
+                                Refunds
+                            </SidebarItem>
+                            <SidebarItem
+                                href="#"
+                                onClick={() => setIsOpen(false)}
+                            >
+                                Shipping
+                            </SidebarItem>
+                        </SidebarCollapse>
+                        <SidebarItem
+                            href="#"
+                            icon={HiInbox}
+                            onClick={() => setIsOpen(false)}
+                        >
+                            Inbox
+                        </SidebarItem>
+                        <SidebarItem
+                            href="#"
+                            icon={HiUser}
+                            onClick={() => setIsOpen(false)}
+                        >
+                            Users
+                        </SidebarItem>
+                        <SidebarItem
+                            href="#"
+                            icon={HiShoppingBag}
+                            onClick={() => setIsOpen(false)}
+                        >
+                            Products
+                        </SidebarItem>
+                        <SidebarItem
+                            href="#"
+                            icon={HiArrowSmRight}
+                            onClick={() => setIsOpen(false)}
+                        >
+                            Sign In
+                        </SidebarItem>
+                        <SidebarItem
+                            href="#"
+                            icon={HiTable}
+                            onClick={() => setIsOpen(false)}
+                        >
+                            Sign Up
+                        </SidebarItem>
+                    </SidebarItemGroup>
+                </SidebarItems>
+            </Sidebar>
+        </div>
     );
 }
